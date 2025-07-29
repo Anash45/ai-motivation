b59114799c1e7c9e3a7f15d0748ef63bd4b5c23e
Syed Anas Bukhari <anas14529@gmail.com>
2025-07-29 06:05:29 +0500

    Logo change,

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 
-<img src="{{asset("assets/images/logo-03/.png")}}" style="height: 50px;" alt="Laravel Logo">
+<img src="{{asset("assets/images/logo-03.png")}}" style="height: 50px;" alt="Laravel Logo">
 </a>
 </td>
 </tr>
