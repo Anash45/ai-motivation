e27ad3728e40e31893b1d69d371e19a41a711cf2
Syed Anas Bukhari <anas14529@gmail.com>
2025-08-03 01:38:27 +0500

    Plan type

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -73,7 +73,7 @@ class SubscriptionController extends Controller
         }
 
         $user = auth()->user();
-        $user->plan_typ = "subscribe";
+        $user->plan_type = "subscribe";
         $user->is_subscribed = true;
         $user->stripe_subscription_id = $subscription->id;
         $user->subscription_ends_at = Carbon::createFromTimestamp($periodEnd);
